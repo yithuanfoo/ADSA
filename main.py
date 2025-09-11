0ae3104b4fa69c4adc9315bbc09a57823db7bc8d
Yit Huan Foo <yithuanyeap@gmail.com>
2025-09-11 16:26:23 +0930

    Added file

--- main.py
+++ main.py
@@ -83,32 +83,17 @@ class AVLTree:
 
         root.height = 1 + max(self.height_get(root.left), self.height_get(root.right))
         balance = self.balance_get(root)
-
-        if balance > 1:
-            if self.balance_get(root.left) >= 0:
-                return self.right_rotate(root)
-            else:
-                root.left = self.left_rotate(root.left)
-                return self.right_rotate(root)
-            
-        if balance < -1:
-            if self.balance_get(root.right) <= 0:
-                return self.left_rotate(root)
-        else:
+        
+        if balance > 1 and self.balance_get(root.left) >= 0:
+            return self.right_rotate(root)
+        if balance > 1 and self.balance_get(root.left) < 0:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+        if balance < -1 and self.balance_get(root.right) <= 0:
+            return self.left_rotate(root)
+        if balance < -1 and self.balance_get(root.right) > 0:
             root.right = self.right_rotate(root.right)
             return self.left_rotate(root)
-            
-        
-        #if balance > 1 and self.balance_get(root.left) >= 0:
-            #return self.right_rotate(root)
-        #if balance > 1 and self.balance_get(root.left) < 0:
-            #root.left = self.left_rotate(root.left)
-            #return self.right_rotate(root)
-        #if balance < -1 and self.balance_get(root.right) < 0:
-            #return self.left_rotate(root)
-        #if balance < -1 and self.balance_get(root.right) >= 0:
-            #root.right = self.right_rotate(root.right)
-            #return self.left_rotate(root)
         
         return root
     
